54bcb3b1b82ed138f4a548cc6c04b61e915eb7fc
Thanishhq <126306372+thanishhw@users.noreply.github.com>
2025-11-20 16:47:20 +0530

    upload project root directory
    demo project for 2e2 deployment  of services

---
+++ demo_hcl/terraform/output.tf
@@ -0,0 +1,15 @@
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "region" {
+  value = var.region
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
---
+++ demo_hcl/terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type        = string
+  default     = "us-west-1"
+}
+
+variable "project" {
+  description = "demo-project"
+  type        = string
+  default     = "demo"
+}
